297ba4e888539656f1375826dc3b55666abeaf97
Kenan Kadic <ken.kadic@outlook.com>
2022-06-20 12:21:53 -0500

    script.js updated

--- script.js
+++ script.js
@@ -28,6 +28,13 @@ const restaurant = {
   },
 };
 
+// OR RETURNS FIRST TRUTHY OPERANT--OR LAST VALUE IF ALL ARE FALSY
+
+// THE AND OPERATOR RETURNS 1ST FALSY VALUE - OR LAST VALUE IF ALL OF THEM ARE TRUTHY
+
+// -- THE OR OPERATOR ---
+console.log('---OR OPERATOR---')
+
 // YOU CAN USE AND and OR to SHORT CIRCUIT things
 
 console.log(3 || 'Jonas');
@@ -54,4 +61,47 @@ restaurant.numGuests = 23;
 
 const guest2 = restaurant.numGuests || 10
 console.log(guest2);
+
+// SHORT CIRCUITING BASED TESTS LIKE THE ONE ABOVE ONLY WORK ON TRUTHY VALUES
+// DON'T COMPARE ZERO TO SOMETHING--ZERO NEVER EXISTS
+
+// --- THE AND OPERATOR ---
+console.log('---AND OPERATOR---')
+
+// THE AND OPERATOR WORKS IN THE OPPOSITE WAY OF OR OPERATOR
+
+// IT PRINTS THE FIRST FALSE VALUE
+
+console.log(0 && 'Jonas')
+// this will print zero
+
+console.log(7 & 'Jonas')
+// this will print 'Jonas'
+
+console.log('Hello' && 23 && null && 'jonas');
+// this will print null
+
+//checking if something exists
+
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach')
+}
+
+// this can replace an if statement! BUT MOST PEOPLE DON'T DO THIS!
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach')
+
+// THE NULLISH COALESCING OPERATOR
+
+restaurant.numGuests = 0
+const guests = restaurant.numGuests || 10;
+console.log(guests)
+// this will give you 10
+
+// THIS OPERATOR IS FROM ES11 or ES2020
+
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+// this gives you the actual zero
+// WHY DOES THIS WORK -- it works with nullish values instead of falsy values--null or undefined
+//this does not include zero or empty string--it is pretending as if these 2 values are true
 
